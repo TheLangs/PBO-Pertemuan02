55ab280ae67220170111167ea32fad5623b037bc
TheLangs <110394681+TheLangs@users.noreply.github.com>
2024-05-01 02:01:15 +0800

    Modifikasi dari InputOutput

--- InputOutput.java
+++ InputOutput.java
@@ -4,8 +4,19 @@ public class InputOutput{
     public static void main(String [] args ){
         Scanner scanner = new Scanner(System.in);
         String nama;
+        int jumlahSks;
+        double IPK;
+
         System.out.print("Masukkan Nama Anda : ");
         nama = scanner.nextLine();
-        System.out.println("Hai, "+nama);
+        
+        System.out.print("Masukkan jumlah SKS yang sudah anda tempuh: ");
+        jumlahSks = scanner.nextInt();
+
+        System.out.print("Masukkan IPK sementara anda: ");
+        IPK = scanner.nextDouble();
+
+        double jumlah = IPK * jumlahSks;
+        System.out.print("Hai, " + nama + ". Total Nilai = " + jumlah);
     }
 }
